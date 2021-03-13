37b2ec6dbbee4360666ccc03c2c7bf2becb1c53e
ronnasqueroquitar <ronnasayd.machado@queroquitar.com.br>
2021-03-13 18:53:04 -0300

    refactor: change login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,73 @@
 <template lang="pug">
-.home-page-wrapper
-  component(is='MyProfile')
+.login-wrapper.tw-grid.tw-h-screen.tw-h-screen.tw-place-content-center
+  .l-card.tw-shadow-lg
+    .l-card-header
+      img.background(src='~/assets/mainBGmobile.jpg', alt='')
+      img.tw-w-20.tw-mr-2.tw-mt-2.logo(
+        src='~/assets/CWT_LOGO_TRASNPARENTE.png'
+      )
+    .l-card-body.tw-p-8.tw-bg-white
+      form
+        fieldset
+          legend.tw-w-full.tw-flex.tw-justify-end Login into your account
+          b-field(label='user', type='', message='')
+            b-input(v-model='user', size='is-small', icon='email')
+          b-field(label='password')
+            b-input(
+              v-model='password',
+              size='is-small',
+              icon='key',
+              type='password'
+            )
+          b-button.tw-w-full.tw-mt-4(type='is-danger') Entrar
 </template>
 
 <script>
 export default {
-  name: 'HomePage',
-  components: {
-    LoginRequest: () => import('@/components/partials/LoginRequest'),
-    MyProfile: () => import('@/components/partials/MyProfile'),
-    ManagerPanel: () => import('@/components/partials/ManagerPanel'),
-    BlockUser: () => import('@/components/partials/BlockUser'),
-    ApprovalDelegation: () =>
-      import('@/components/partials/ApprovalDelegation'),
-    AssignmentOfProfile: () =>
-      import('@/components/partials/AssignmentOfProfile'),
-    ProfileSearch: () => import('@/components/partials/ProfileSearch'),
-    AssignmentOfProfile2: () =>
-      import('@/components/partials/AssignmentOfProfile2'),
-    MaintainProfile: () => import('@/components/partials/MaintainProfile'),
-    MaintainSystem: () => import('@/components/partials/MaintainSystem'),
-    MaintainSystem2: () => import('@/components/partials/MaintainSystem2'),
-    MaintainSystem3: () => import('@/components/partials/MaintainSystem3'),
-    AlternateApproverRegister: () =>
-      import('@/components/partials/AlternateApproverRegister'),
-  },
+  name: 'Teste',
+  components: {},
+  layout: 'public',
+  props: {},
   data() {
-    return {}
+    return { user: '', password: '' }
   },
   computed: {},
   watch: {},
   mounted() {},
-  created() {
-    this.$nuxt.$emit('menuType', 'ManagerMenu')
-  },
+  created() {},
   methods: {},
 }
 </script>
 
 <style lang="scss" scoped>
-.home-page-wrapper {
-  width: 100%;
-  height: 100%;
+.login-wrapper {
+  margin: 0 px2rem(10);
+  .l-card {
+    max-width: px2rem(375);
+    width: 100%;
+  }
+  .l-card-body {
+    border-radius: 0 0 px2rem(8) px2rem(8);
+  }
+  .l-card-header {
+    display: flex;
+    position: relative;
+    img.background {
+      flex: 1;
+      border-radius: px2rem(8) px2rem(8) 0 0;
+    }
+    img.logo {
+      position: absolute;
+      right: 0;
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+.login-wrapper {
+  label {
+    font-size: 0.75rem;
+  }
 }
 </style>
