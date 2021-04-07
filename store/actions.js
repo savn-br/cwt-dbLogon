8001137da83b650b710031fcefdac39e678c42f0
ronnasqueroquitar <ronnasayd.machado@queroquitar.com.br>
2021-04-07 01:26:11 -0300

    refactor: add try catch to actions

--- store/actions.js
+++ store/actions.js
@@ -1,48 +1,68 @@
 export default {
   async enableAccess({ state, commit }) {
-    const { data: responseData } = await this.$axios.post(
-      `/access/${state.userData.userId}`
-    )
-    commit('changeUserStatus', responseData.data.status)
-    commit('changeUserData', responseData.data)
+    try {
+      const { data: responseData } = await this.$axios.post(
+        `/access/${state.userData.userId}`
+      )
+      commit('changeUserStatus', responseData.data.status)
+      commit('changeUserData', responseData.data)
+    } catch (error) {
+      console.error(error)
+    }
   },
   async updateAccess({ state, commit }) {
-    const { data: responseData } = await this.$axios.put(
-      `/access/${state.userData.userId}`,
-      {
-        ...state.userData,
-      }
-    )
-    commit('changeUserStatus', responseData.data.status)
-    commit('changeUserData', responseData.data)
+    try {
+      const { data: responseData } = await this.$axios.put(
+        `/access/${state.userData.userId}`,
+        {
+          ...state.userData,
+        }
+      )
+      commit('changeUserStatus', responseData.data.status)
+      commit('changeUserData', responseData.data)
+    } catch (error) {
+      console.error(error)
+    }
   },
 
   async getAccess({ state, commit }) {
-    const { data: responseData } = await this.$axios.get(
-      `/access/${state.userData.userId}`
-    )
-    commit('changeUserStatus', responseData.data.status)
-    commit('changeUserData', responseData.data)
+    try {
+      const { data: responseData } = await this.$axios.get(
+        `/access/${state.userData.userId}`
+      )
+      commit('changeUserStatus', responseData.data.status)
+      commit('changeUserData', responseData.data)
+    } catch (error) {
+      console.error(error)
+    }
   },
 
   async updateMyProfile({ state, commit }) {
-    const { data: responseData } = await this.$axios.put(
-      `/myProfile/${state.userData.userId}`,
-      {
-        ...state.userData,
-      }
-    )
-    commit('changePointOfSales', responseData.data.pointOfSales)
-    commit('changeProfileAccess', responseData.data.profileAccess)
-    commit('changeUserData', responseData.data)
+    try {
+      const { data: responseData } = await this.$axios.put(
+        `/myProfile/${state.userData.userId}`,
+        {
+          ...state.userData,
+        }
+      )
+      commit('changePointOfSales', responseData.data.pointOfSales)
+      commit('changeProfileAccess', responseData.data.profileAccess)
+      commit('changeUserData', responseData.data)
+    } catch (error) {
+      console.error(error)
+    }
   },
 
   async getMyProfile({ state, commit }) {
-    const { data: responseData } = await this.$axios.get(
-      `/myProfile/${state.userData.userId}`
-    )
-    commit('changePointOfSales', responseData.data.pointOfSales)
-    commit('changeProfileAccess', responseData.data.profileAccess)
-    commit('changeUserData', responseData.data)
+    try {
+      const { data: responseData } = await this.$axios.get(
+        `/myProfile/${state.userData.userId}`
+      )
+      commit('changePointOfSales', responseData.data.pointOfSales)
+      commit('changeProfileAccess', responseData.data.profileAccess)
+      commit('changeUserData', responseData.data)
+    } catch (error) {
+      console.error(error)
+    }
   },
 }
